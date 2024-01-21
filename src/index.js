e6510a81e649ad8b2bebc6bbee0bcec9d9c8b30b
utsav <utsavmahato05@gmail.com>
2024-01-21 06:50:11 +0545

    comment removed

--- src/index.js
+++ src/index.js
@@ -5,17 +5,14 @@ import About from "./Components/About";
 import Startup from "./Components/Startup";
 import Contact from "./Components/Contact";
 import TechStack from "./Components/TechStack";
-import Projects from "./Components/Projects";
-import MyDetails from "./Components/MyDetails";
+import Projects from "./Components/Projects"; 
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-  {/* <App/> */}
   <About/>
   <Startup/>
-  {/* <MyDetails/> */}
   <Projects/>
   <TechStack/>
   <Contact/>
